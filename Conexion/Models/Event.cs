541a0382cfcdd1e7bac506542a8c43b6f5ba68be
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-10-06 09:48:50 -0400

    feat: new model for Event

---
+++ Conexion/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conexion.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public int Capacity { get; set; }
+    }
+}
