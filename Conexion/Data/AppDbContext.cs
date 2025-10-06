c6c21a2b6bae33aeb719bc04d150102f61956a26
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-10-06 10:50:48 -0400

    feat: new appdbcontext

---
+++ Conexion/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Conexion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conexion.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books => Set<Book>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(b =>
+            {
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Title).IsRequired().HasMaxLength(200);
+                b.Property(x => x.Year).IsRequired();
+            });
+        }
+    }
+}
