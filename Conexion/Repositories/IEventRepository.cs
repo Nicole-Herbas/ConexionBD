4c11999fae5615624dbdca79fc3aae1687e02c2d
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-10-06 11:10:40 -0400

    feat: new I Event Repository

--- Conexion/Repositories/IEventRepository.cs
+++ Conexion/Repositories/IEventRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Conexion.Repositories
+﻿using Conexion.Models;
+
+namespace Conexion.Repositories
 {
     public interface IEventRepository
     {
+        Task<IEnumerable<Event>> GetAll();
+        Task<Event?> GetById(Guid id);
+        Task Add(Event eve);
+        Task Delete(Guid id);
     }
 }
