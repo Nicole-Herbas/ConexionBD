981db4004e7f0964e06f749b9e24e70c7d00cadc
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-10-06 09:54:18 -0400

    feat: update dto for event

---
+++ Conexion/Models/dtos/UpdateEventDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conexion.Models.dtos
+{
+    public class UpdateEventDto
+    {
+        [Required, StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+        
+        public DateTime Date { get; set; }
+
+        [Range(1, 2100)]
+        public int Capacity { get; set; }
+    }
+}
