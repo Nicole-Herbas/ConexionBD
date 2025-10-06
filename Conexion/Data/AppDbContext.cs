cbee279f5affdd76e908c6f7c388b74c87e79a04
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-10-06 11:02:53 -0400

    feat: new appDbContext

--- Conexion/Data/AppDbContext.cs
+++ Conexion/Data/AppDbContext.cs
@@ -9,15 +9,16 @@ namespace Conexion.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Event> Books => Set<Event>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Book>(b =>
+            modelBuilder.Entity<Event>(b =>
             {
                 b.HasKey(x => x.Id);
                 b.Property(x => x.Title).IsRequired().HasMaxLength(200);
-                b.Property(x => x.Year).IsRequired();
+                b.Property(x => x.Capacity).IsRequired();
+                b.Property(x => x.Date).IsRequired();
             });
         }
     }
